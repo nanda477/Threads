390c0ad843f021df0887513e78ae0dcfd77bd8e8
Nandakishore <nanda.kishore@mahiti.org>
2018-12-27 18:08:47 +0530

    FC

---
+++ Threads/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Threads
+//
+//  Created by subramanyam on 24/12/18.
+//  Copyright © 2018 mahiti. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
